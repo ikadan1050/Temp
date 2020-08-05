6e15317dde2779272909bd5c17f4f1cdcc7e3043
Oluwatosin <69093030+Tjesctacy@users.noreply.github.com>
2020-08-05 21:42:58 +0100

    Update oluwatosin.R

--- oluwatosin.R
+++ oluwatosin.R
@@ -1,10 +1,10 @@
-Name <- c("Jegede Oluwatosin Ayodeji")
+Name <- ("Jegede Oluwatosin Ayodeji")
 Name
-Email <- "tjesctacy@gmail.com"
+Email <- ("tjesctacy@gmail.com")
 Email
-Language <- "R programming"
+Language <- ("R programming")
 Language
-Biostack <- "Genomics"
+Biostack <- ("Genomics")
 Biostack
-Slack_Username <- "@Oluwatosin"
+Slack_Username <- ("@Oluwatosin")
 Slack_Username
