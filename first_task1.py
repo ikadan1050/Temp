d2c4fbe643ed5a51cf6a8945a2577000b00297bf
ade152 <69103952+ade152@users.noreply.github.com>
2020-08-09 22:06:13 +0100

    Update first_task1.py

--- first_task1.py
+++ first_task1.py
@@ -1,10 +1,6 @@
-name = 'Adeoye'
+name = 'adeoye olude'
 email = 'adeoyeifeoluwa@ymail.com'
-language = 'Python'
-biostack = 'Data Analysis'
+language = 'python'
+biostack = 'data Analysis'
 slack_username = '@adeoye'
-print ('My name is',name)
-print ('My email address is', email)
-print ('I wrote my script with', language)
-print ('My biostack is', biostack)
-print ('My slack_username is',slack_username)
+print ('adeoye olude', '@adeoye', 'python', 'data analysis','adeoyeifeoluwa@ymail.com')
